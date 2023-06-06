ce879aa7b9a985e6eb09a9b3c90c06a180a45ea5
iaal96 <ivan.anguiano17@hotmail.com>
2023-06-06 17:55:36 -0600

    Updated app

--- app.py
+++ app.py
@@ -1,166 +1,114 @@
-from flask import Flask, jsonify
 import numpy as np
-import datetime as dt
+
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import datetime as dt
+from flask import Flask, jsonify
 
-
-#################################################
 # Database Setup
-#################################################
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+engine = create_engine('sqlite:///Resources/hawaii.sqlite', connect_args={'check_same_thread': False}, echo=True)
 
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+# Create our session (link) from Python to the DB
+session = Session(engine)
 
-# Save reference to the table
-measurement = Base.classes.measurement
-station = Base.classes.station
-
-#################################################
 # Flask Setup
-#################################################
 app = Flask(__name__)
 
-#################################################
-# Flask Setup
-#################################################
-app = Flask(__name__)
-
-
-#################################################
 # Flask Routes
-#################################################
-
-# Create your welcome endpoint with the several different routes
-@app.route('/')
+#List all routes that are available
+@app.route("/")
 def welcome():
-    return(
-        f"Weclome to the Hawaiian climate API!<br/>"
-        f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/start/2010-01-01<br/>"
-        f"/api/v1.0/start/2010-01-01/end/2017-08-23"
-    )
 
-@app.route('/api/v1.0/precipitation')
-def precipitation():
-    
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    # Query date and prcp from measurements table
-    date_prcp = session.query(measurement.date, measurement.prcp).all()
-
-    session.close()
-    
-    # Create dictionary with dates as keys and prcp as values
-    x = []
-    for date, prcp in date_prcp:
-        date_dict = {}
-        date_dict[date] = prcp
-        x.append(date_dict)
-    return jsonify(x)
+     return (
+         f"/api/v1.0/precipitation<br/>"
+         f"/api/v1.0/stations<br/>"
+         f"/api/v1.0/tobs<br/>"
+         f"/api/v1.0/<start><br/>"
+         f"/api/v1.0/<start>/<end><br/>"
+     )
 
 
-@app.route('/api/v1.0/stations')
+@app.route("/api/v1.0/precipitation")
+def precip():
+    # Calculate the date 1 year ago from the last data point in the database
+    last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
+
+    #find date 12 months before
+    last_year_date = dt.datetime.strptime(last_date, "%Y-%m-%d") - dt.timedelta(days=366)
+
+    # Perform a query to retrieve the data and precipitation scores
+    results = query = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= last_year_date).all()
+
+    rain_season = []
+    for date, prcp in results:
+        data = {}
+        data['date'] = date
+        data['prcp'] = prcp
+        rain_season.append(data)
+
+    return jsonify(rain_season)
+
+
+@app.route("/api/v1.0/stations")
 def stations():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
+    
+    results = session.query(Station.name, Station.station, Station.elevation).all()
 
-    # Query the entire  station table
-    station_tuple = session.query(station.station).all()
-
-    session.close()
-
-    # Convert list of tuples into normal list
-    station_list = list(np.ravel(station_tuple))
-
+    #create dictionary for JSON
+    station_list = []
+    for result in results:
+        row = {}
+        row['name'] = result[0]
+        row['station'] = result[1]
+        row['elevation'] = result[2]
+        station_list.append(row)
     return jsonify(station_list)
 
-@app.route('/api/v1.0/tobs')
-def most_active():
+@app.route("/api/v1.0/tobs")
+def temperature_tobs():
+    results = session.query(Station.name, Measurement.date, Measurement.tobs).\
+        filter(Measurement.date >= "2017-01-01", Measurement.date <= "2018-01-01").\
+        all()
 
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
+    #use dictionary, create json
+    tobs_list = []
+    for result in results:
+        row = {}
+        row["Date"] = result[1]
+        row["Station"] = result[0]
+        row["Temperature"] = int(result[2])
+        tobs_list.append(row)
 
-    # Starting from the most recent data point in the database. 
-    latest_date = dt.date(2017,8, 23)
+    return jsonify(tobs_list)
 
-    # Calculate the date one year from the last date in data set.
-    year_ago =  latest_date - dt.timedelta(days = 365)
 
-    # Query the entire tobs for the most active station in the last year
-    last_12 = session.query(measurement.date, measurement.tobs).filter_by(station = "USC00519281").\
-    filter(measurement.date >= year_ago).all()
-
-    session.close()
-
-    last_12_list = []
-    for date, tobs in last_12:
-        tobs_dict = {}
-        tobs_dict[date] = tobs
-        last_12_list.append(tobs_dict) 
-    return jsonify(last_12_list)     
-
-@app.route('/api/v1.0/start/<start>')
-def start_date(start):
-     # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    # Query the date greater than or equal to start
-    sel = [func.min(measurement.tobs),
-           func.avg(measurement.tobs),
-           func.max(measurement.tobs)
-    ]
+@app.route("/api/v1.0/<start>")
+def start(start=None):
     
-    
-    start_filter = session.query(*sel).filter(measurement.date >= start).all()
-    start_list = [
-        {"TMIN": start_filter[0][0]},
-        {"TAVG": start_filter[0][1]},
-        {"TMAX": start_filter[0][2]}
-    ]
-    if start <= '2017-08-23':
-        return jsonify(start_list)
-    else:
-        return jsonify("Error: start date past time horizon, please enter a date before 2017-08-23'")
+    from_start = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs),
+                               func.max(Measurement.tobs)).filter(Measurement.date >= start).group_by(
+        Measurement.date).all()
+    from_start_list = list(from_start)
+    return jsonify(from_start_list)
 
-    session.close()
 
-@app.route('/api/v1.0/start/<start>/end/<end>')
-def start_end(start, end):
-     # Create our session (link) from Python to the DB
-    session = Session(engine)
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start=None, end=None):
+    between_dates = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs),
+                                  func.max(Measurement.tobs)).filter(Measurement.date >= start).filter(
+        Measurement.date <= end).group_by(Measurement.date).all()
+    between_dates_list = list(between_dates)
+    return jsonify(between_dates_list)
 
-    # Query the date greater than or equal to start
-    sel = [func.min(measurement.tobs),
-           func.avg(measurement.tobs),
-           func.max(measurement.tobs)
-    ]
-    
-    
-    start_end_filter = session.query(*sel).\
-        filter(measurement.date.between(start,end)).all()
-    start_end_list = [
-        {"TMIN": start_end_filter[0][0]},
-        {"TAVG": start_end_filter[0][1]},
-        {"TMAX": start_end_filter[0][2]}
-    ]
-    if (start <= '2017-08-23') and (end >='2010-01-01') :
-        return jsonify(start_end_list)
-    else:
-        return jsonify("Error: start and end date not within time horzion, please enter a start and end date between 2010-01-01 : 2017-08-23")
-    
-
- 
-
-    session.close()
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
